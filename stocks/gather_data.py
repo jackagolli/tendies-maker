afccf30b2be9a3a2d166117fc4805a43d698438c
Jack <jack.agolli@nasa.gov>
2020-02-10 15:26:44 -0600

    added files

--- stocks/gather_data.py
+++ stocks/gather_data.py
@@ -1,11 +1,13 @@
 import datetime
 import yfinance as yf
-
+import matplotlib.pyplot as plt
 
 
 
 companyTicker = 'AMD'
 company = yf.Ticker(companyTicker)
 historicalData = company.history(period="1yr")
-print(company.info)
 
+data = yf.download('AAPL','2016-01-01','2018-01-01')
+data.Close.plot()
+plt.show()
