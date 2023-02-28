bebe97574daa716a60d0841058772fc1e2e29e3c
jackagolli <jagolli192@gmail.com>
2023-02-28 12:14:12 -0600

    bruh

--- jobs/generate_data.py
+++ jobs/generate_data.py
@@ -1,10 +1,10 @@
 from loguru import logger
 
 from src.tendies_maker.datamodel import TrainingData
-print('ran')
-# logger.info("Instantiate training data by scraping WSB, looking for 5% spike in last 30 days")
-# td = TrainingData(tgt_pct=0.05, tgt_days=30)
-# td.append_all()
-# logger.info("All data generated, writing to database")
-# td.write_data()
-# logger.info("Sucessfully written to db")
+
+logger.info("Instantiate training data by scraping WSB, looking for 5% spike in last 30 days")
+td = TrainingData(tgt_pct=0.05, tgt_days=30)
+td.append_all()
+logger.info("All data generated, writing to database")
+td.write_data()
+logger.info("Sucessfully written to db")
